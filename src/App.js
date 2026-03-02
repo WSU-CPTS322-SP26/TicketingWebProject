c3845af3eb89535b5daf81c85020be580ba6b05f
rdas1501 <rdas1805@gmail.com>
2026-03-01 20:52:16 -0800

    Added Tickr frontend project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TickrAnimation from './TickrAnimation';
+import HomePage from './HomePage';
 
 function App() {
+  const [showHomePage, setShowHomePage] = useState(false);
+
+  const handleAnimationComplete = () => {
+    setTimeout(() => {
+      setShowHomePage(true);
+    }, 1000);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!showHomePage ? (
+        <TickrAnimation onComplete={handleAnimationComplete} />
+      ) : (
+        <HomePage />
+      )}
     </div>
   );
 }
