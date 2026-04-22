0fd187392d8c9470326624d5d7de1b0d24d2f84b
Alex Deng <137854871+Clowny2023@users.noreply.github.com>
2026-04-21 18:40:13 -0700

    Update AuthController.java

--- backend/src/main/java/edu/wsu/cpts322/project/backend/controllers/auth/AuthController.java
+++ backend/src/main/java/edu/wsu/cpts322/project/backend/controllers/auth/AuthController.java
@@ -17,6 +17,11 @@ package edu.wsu.cpts322.project.backend.controllers.auth;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api")
